3056d2c8ad8221abe4b76952b338f9255c8e847f
Melvin Josue Gutierrez Amaya <98471989+melvingutierrez20@users.noreply.github.com>
2024-10-09 15:58:17 -0600

    Update Views.php

--- Config/App/Views.php
+++ Config/App/Views.php
@@ -1,14 +1,16 @@
 <?php
-class Views{
- 
-    public function getView($ruta, $vista, $data="")
-    {
-        if ($ruta == "home") {
-            $vista = "Views/".$vista.".php";
-        }else{
-            $vista = "Views/".$ruta."/".$vista.".php";
+class Views {
+    public function getView($ruta, $vista, $data="") {
+        // Construcción de la ruta completa
+        $vista = "Views/" . $ruta . "/" . $vista . ".php";
+
+        // Verificar si la vista existe antes de incluirla
+        if (file_exists($vista)) {
+            require $vista;
+        } else {
+            // Mostrar un mensaje de error si el archivo no existe
+            echo "Error: la vista $vista no fue encontrada.";
         }
-        require $vista;
     }
 }
 ?>
