a832fd24052d76253e7acb4b21ba298be0181095
Elvis Reyluis Gutierrez Hernandez <147088986+elvisGutierrez22@users.noreply.github.com>
2024-10-09 13:15:21 -0600

    Update Conexion.php

--- Config/App/Conexion.php
+++ Config/App/Conexion.php
@@ -3,7 +3,7 @@ class Conexion{
     private $conect;
     public function __construct()
     {
-        $pdo = "mysql:=unix_socket=/cloudsql/tiendaonline-436721:us-central1:tiendaonline2024;dbname=".DB.";".CHARSET;
+        $pdo = "mysql:host=".HOST.";dbname=".DB.";".CHARSET;
         try {
             $this->conect = new PDO($pdo, USER, PASS);
             $this->conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
