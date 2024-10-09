e5a24a08df0d3fd5e12af866249c111c5ada11cb
Melvin Josue Gutierrez Amaya <98471989+melvingutierrez20@users.noreply.github.com>
2024-10-09 15:16:58 -0600

    Update Conexion.php

--- Config/App/Conexion.php
+++ Config/App/Conexion.php
@@ -1,36 +1,20 @@
 <?php
-class Conexion {
+class Conexion{
     private $conect;
-
-    public function __construct() {
-        // Nombre de la conexión de Cloud SQL
-        $connection_name = 'tiendaonline-436721:us-central1:tiendaonline2024';
-        // Nombre de la base de datos
-        $dbname = 'tienda_online'; // Reemplaza con tu base de datos
-        // Usuario y contraseña de la base de datos
-        $username = 'root';               // Reemplaza con tu usuario de la base de datos
-        $password = 'administracion2024';            // Reemplaza con tu contraseña de la base de datos
-
-        // Configuración del DSN para usar el socket UNIX
-        $dsn = sprintf(
-            'mysql:unix_socket=/cloudsql/tiendaonline-436721:us-central1:tiendaonline',
-            $connection_name,
-            $dbname
-        );
-
+    public function __construct()
+    {
+        $pdo = "mysql:host=".HOST.";dbname=".DB.";".CHARSET;
         try {
-            // Crear la conexión usando PDO
-            $this->conect = new PDO($dsn, $username, $password);
+            $this->conect = new PDO($pdo, USER, PASS);
             $this->conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Conexión exitosa a la base de datos";
         } catch (PDOException $e) {
-            // Manejar los errores de conexión
-            echo "Error en la conexión: " . $e->getMessage();
+            echo "Error en la conexion".$e->getMessage();
         }
     }
-
-    public function conect() {
+    public function conect()
+    {
         return $this->conect;
     }
 }
+ 
 ?>
